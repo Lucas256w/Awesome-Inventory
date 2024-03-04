8abec70264368d63dd0ea5451fda36814fadee7c
Jiahao <lucashuamani256w@gmail.com>
2024-03-04 01:51:45 -0500

    Implement models

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const multer = require("multer");
 
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 
 const app = express();
+
+// storing image on servers disk with multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 //connect to MongoDB
 const mongoose = require("mongoose");
---
+++ models/item.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  profile_img: String,
+  cloudinary_id: String,
+  price: { type: Schema.Types.Decimal128, required: true },
+  number_in_stock: { type: Number, required: true },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/inventory/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
